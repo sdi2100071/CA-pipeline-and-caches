9947f65157cf8ed7a70eaa35e00fd544ce4ba66b
sdi2100071 <mikaelakullolli123@gmail.com>
2024-12-15 17:46:50 +0200

    created square matrix mul scalar program(no optimisations)

--- caches/matrix_scalar.s
+++ caches/matrix_scalar.s
@@ -6,14 +6,14 @@
 #################################################
 
 #################################################
-#     [ An x s ]                                #
-#     vector x scalar                           #
+#     [ A_n*n x s ]                             #
+#     square matrix x scalar                    #
 #################################################
 
 #################################################
 # $s0 --> scalar                                #
 # $s1 --> N                                     #
-# $a0 --> vector                                #
+# $a0 --> array                                 #
 # $t2 --> current vector element                #
 # $s3 --> result                                #
 #################################################
@@ -28,17 +28,17 @@
     .globl __start  
                                 
 __start:
-    la $t0, n        # Load N - vector dim
+
+    la $t0, n        # Load N
     lw $s1, 0($t0)  
     lw $s0, 4($t0)   # Load scalar
 
-    la $a0, vector   # Load vector
+    la $a0, array   # Load array
     la $s3, result   # Load result
 
-loop:
-
-    beq $s1, $zero, Exit 
-    lw $t2, 0($a0)          # Load vector element
+outer:
+    beq $s1, $zero, reinit 
+    lw $t2, 0($a0)          # Load element
     
     mult $t2, $s0
 
@@ -52,7 +52,31 @@ loop:
     addi $a0, $a0, 4        # Load address of next element
     addi $s1, $s1, -1       # Reduce element counter
 
-    beq $t6, $t7, loop
+    beq $t6, $t7, outer
+
+Exit1:   
+        li $v0, 10
+        syscall 
+
+reinit:
+    lw $s1, 0($t0)  
+inner: 
+    beq $s1, $zero, Exit 
+    lw $t2, 0($a0)          # Load element
+    
+    mult $t2, $s0
+
+    # Check for arithmetic overflow.
+    mfhi $t6
+    mflo $t7
+    sw $t7, 0($s3)          # Store result.
+    sra $t7, $t7, 31
+
+    addi $s3, $s3, 4        # Load address of next result
+    addi $a0, $a0, 4        # Load address of next element
+    addi $s1, $s1, -1       # Reduce element counter
+
+    beq $t6, $t7, inner
 
 Exit:   
         li $v0, 10
@@ -65,10 +89,10 @@ Exit:
 #################################################
 
     .data
-n: .word 8
-scalar: .word 20000
-vector: .word 1, 2, 3, 4, 5, 6, 7, 8
-result: .space 32
+n: .word 4
+scalar: .word 3
+array: .word 1, 2, 3, 4, 5, 6, 7, 8
+result: .space 1000
 
 
 
